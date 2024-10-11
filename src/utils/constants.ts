a6522ca31783e5617aba57ca627e62c33618a7c7
mabozed <mabozed.com@gmail.com>
2024-10-11 18:49:01 +0300

    add URL

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-const PRODUCTION_DOMAIN = 'https://vica-next-course.vercel.app/'
+const PRODUCTION_DOMAIN = 'https://vica-twitter-api.vercel.app/'
 const DEVELOPMENT_DOMAIN = 'http://localhost:3000'
 
 export const DOMAIN =
